81daef516fa7eaaef2c1d514b87834d07b89fc6e
FonKnp <fonnfonk@gmail.com>
2024-05-03 16:30:54 +0200

    fixed typo

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,14 +13,14 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    productId: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "product",
         key: "id",
       },
     },
-    tagID: {
+    tag_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "tag",
